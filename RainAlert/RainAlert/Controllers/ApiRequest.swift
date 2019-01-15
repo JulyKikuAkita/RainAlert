83cebe48debcab00cdfda2e596079df086c32038
julykikuakita <b92701105@gmail.com>
2019-01-15 15:39:13 -0800

    Added zipcode textfield and button

---
+++ RainAlert/RainAlert/Controllers/ApiRequest.swift
@@ -0,0 +1,22 @@
+//
+//  ApiRequest.swift
+//  RainAlert
+//
+//  Created by July on 1/15/19.
+//  Copyright © 2019 July. All rights reserved.
+//
+
+// MARK: Reading https://matteomanferdini.com/network-requests-rest-apis-ios-swift/
+
+//MARK: Weather API
+// https://www.weather.gov/documentation/services-web-api
+//Json format https://api.weather.gov/gridpoints/TOP/31,80/forecast/hourly
+
+// need to check if it's free
+// https://developer.here.com/api-explorer/rest/auto_weather/weather-observation-zipcode
+// https://developer.here.com/projects/PROD-048aeff5-06da-46d2-9af5-5b00a86a4256
+
+import Foundation
+class ApiRequest {
+    
+}
--- RainAlert/RainAlert/ViewController.swift
+++ RainAlert/RainAlert/ViewController.swift
@@ -8,18 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    //MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var zipcodeLabel: UILabel!
+    @IBOutlet weak var setDefaultLabelText: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Handle the text field’s user input through delegate callbacks.
+        nameTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        zipcodeLabel.text = "Query Zipcode: \(textField.text ?? "-1")"
+    }
+    
+    //MARK: Actions
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        zipcodeLabel.text = "Default"
+    }
 }
 
