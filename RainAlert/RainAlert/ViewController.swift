91ea9c4bf16ec2c095ecfbcc65c8f079f90e60b9
JulyKikuAkita <i-fang.lee@grindr.com>
2019-01-15 00:55:07 -0800

    build rain alert prj

---
+++ RainAlert/RainAlert/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RainAlert
+//
+//  Created by July on 1/14/19.
+//  Copyright © 2019 July. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
